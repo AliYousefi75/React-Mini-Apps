9f7163bb812756960f91fec0c1eec6151bb90c84
ali <ali.yousefi7518@gmail.com>
2026-08-16 19:39:56 +0430

    todo-list(add App)

---
+++ todo-list/src/App.jsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import TaskForm from "./components/TaskForm";
+import TaskItem from "./components/TaskItem";
+
+
+function App(){
+
+  const [items , setItems] = useState([
+        {id:1, title:'Learn html'},
+        {id:2, title:'Learn css'},
+        {id:3,title:'Learn js'},
+    ])
+
+    function handleDelete(id){
+       setItems(items.filter((item)=>item.id !== id))
+    }
+
+    function handleAddTask(title){
+      const newItem = {
+        id:Date.now(),
+        title:title,
+      }
+      setItems([...items,newItem])
+    }
+
+
+  return(
+    <div 
+    style={{
+      backgroundColor:"#ddfdff",
+      maxWidth:'600px',
+      margin:'auto',
+      borderRadius:'30px',
+      padding:'30px',
+      display:'flex',
+      flexDirection:'column',
+      justifyContent:"center",
+      alignItems:'center'
+    }}
+    >
+      <h1
+      style={{letterSpacing:'5px'}}
+      >TO DO LIST</h1>
+
+      <TaskForm onAddTask={handleAddTask}/>
+
+      {items.length === 0 ?(<p style={{marginTop:'10px'}}>your toDo list is empty</p>):(
+        items.map((item)=>(
+        <TaskItem 
+        title={item.title} 
+        key={item.id}
+        onDelete={()=>handleDelete(item.id)}/>
+      ))
+      )}
+      
+
+    </div>
+  )
+}
+
+export default App;
