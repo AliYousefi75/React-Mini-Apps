dfd31b3374455c643eb9cb2139f2d66e47810101
ali <ali.yousefi7518@gmail.com>
2026-08-26 21:42:26 +0430

    tic-tac-toe-game(updating object and array state immutably)

--- tic-tac-toe-game/src/components/GameBoard.jsx
+++ tic-tac-toe-game/src/components/GameBoard.jsx
@@ -1,4 +1,4 @@
-
+import { useState } from "react"
 
 
 const initialGameBoard =[
@@ -8,11 +8,24 @@ const initialGameBoard =[
 ]
 
 export default function GameBoard(){
+
+    const [gameBoard,setGameBoard] = useState(initialGameBoard);
+
+    function handleSelectSquare(rowIndex,colIndex){
+        setGameBoard((prevGameBoard)=>{
+            const updatedBoard = [...prevGameBoard.map(innerArray=>[...innerArray])];
+            updatedBoard[rowIndex][colIndex] = 'X';
+            return updatedBoard;
+        })
+    }
+
     return (
         <ol id="game-board">
-            {initialGameBoard.map((row,rowIndex)=><li key={rowIndex}>
+            {gameBoard.map((row,rowIndex)=><li key={rowIndex}>
                 <ol>
-                    {row.map((playerSymbol,colIndex)=> <li key={colIndex}><button>{playerSymbol}</button></li>)}
+                    {row.map((playerSymbol,colIndex)=> <li key={colIndex}>
+                        <button onClick={()=>handleSelectSquare(rowIndex,colIndex)}>{playerSymbol}</button>
+                        </li>)}
                 </ol>
             </li>)}
         </ol>
