65b98c91fbd2b6d22252f02092b88894f6ca18be
ali <ali.yousefi7518@gmail.com>
2026-09-02 20:36:47 +0430

    tic-tac-toe-game(reducing state management)

--- tic-tac-toe-game/src/App.jsx
+++ tic-tac-toe-game/src/App.jsx
@@ -3,18 +3,31 @@ import GameBoard from "./components/GameBoard"
 import Log from "./components/Log"
 import { useState } from "react"
 
+
+
+function derivedActivePlayer(gameTurns){
+  let currentPlayer ="X";
+      if(gameTurns.length>0 && gameTurns[0].player === 'X'){
+        currentPlayer = "O"
+      }
+      return currentPlayer;
+
+}
+
+
+
 function App() {
 
   const [gameTurns ,setGameTurns] = useState([]);
-  const [activePlayer, setActivePlayer] = useState('X')
+  // const [activePlayer, setActivePlayer] = useState('X')
+
+  const activePlayer = derivedActivePlayer(gameTurns);
   
   function handleSelectSquare(rowIndex,colIndex){
-    setActivePlayer((currActivePlayer)=>currActivePlayer === 'X'? 'O' : 'X'); 
+    // setActivePlayer((currActivePlayer)=>currActivePlayer === 'X'? 'O' : 'X'); 
     setGameTurns(prevTurns => {
-      let currentPlayer ="X";
-      if(prevTurns.length>0 && prevTurns[0].player === 'X'){
-        currentPlayer = "O"
-      }
+      const currentPlayer = derivedActivePlayer(prevTurns);
+      
       const updatedTurns = [{square:{row:rowIndex , col:colIndex},player:activePlayer},...prevTurns]
       return updatedTurns;
     });
