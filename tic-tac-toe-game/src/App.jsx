3b7d89f2c2a6eef0b7f9136617e3027877bf03f7
ali <ali.yousefi7518@gmail.com>
2026-09-07 16:59:17 +0430

    tic-tac-toe-game(when not to lift state up)

--- tic-tac-toe-game/src/App.jsx
+++ tic-tac-toe-game/src/App.jsx
@@ -23,7 +23,7 @@ function derivedActivePlayer(gameTurns){
 
 
 function App() {
-
+  const[player , setPlayers] = useState({"X":'Player 1' , 'O':'Player 2'})
   const [gameTurns ,setGameTurns] = useState([]);
   //const [hasWinner,setHasWinner] = useState(false)
   // const [activePlayer, setActivePlayer] = useState('X')
@@ -67,6 +67,15 @@ function App() {
 
   function handleRestart(){
     setGameTurns([]);
+  }
+
+  function handlePlayerNameChange(symbol,newName){
+    setPlayers(prevPlayers=>{
+      return{
+        ...prevPlayers,
+        [symbol]:newName
+      };
+    });
   }
 
   return (
